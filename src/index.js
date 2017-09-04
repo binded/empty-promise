fba95cceb3546bca3703b081559d9a31eaffb2e7
Sky Higgins <eveningsky@gmail.com>
2017-09-04 14:30:48 -0400

    getter indicates if already res/rej (with tests)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 module.exports = () => {
   let callbacks
+  let done = false
+
   const p = new Promise((resolve, reject) => {
     callbacks = { resolve, reject }
   })
+
+  p.done = () => done
   p.resolve = (val) => {
     callbacks.resolve(val)
+    done = true
     return p
   }
   p.reject = (val) => {
     callbacks.reject(val)
+    done = true
     return p
   }
+
   return p
 }
